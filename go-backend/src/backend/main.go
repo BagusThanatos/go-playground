f66f1020c9e8d5a9c7b73752ce32829db3638f2c
Bagus Nurtomo <bagusthanatos@gmail.com>
2018-11-12 07:21:31 +0700

    Hello endpoint

--- go-backend/src/backend/main.go
+++ go-backend/src/backend/main.go
@@ -2,9 +2,40 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"log"
+	"flag"
+	"encoding/json"
 )
 
+type Data struct {
+	Title string
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	data := Data{}
+	data.Title = "HELLO WORLD"
+	payload, err := json.Marshal(data)
+	if err!=nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(data.Title)
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(payload)
+}
 
 func main() {
-	fmt.Println("HELLO WORLD!")
+	flag.Parse()
+	args := flag.Args()
+	http.HandleFunc("/hello", Hello)
+
+	port := "8080"
+	if len(args) > 0 {
+		port = args[0]
+	}
+
+	fmt.Printf("Listening in port %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
